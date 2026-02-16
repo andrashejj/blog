174883acbb567596a11e8d4f81d3befbcc8483b4
Andras Hejj <andras@pierwallet.com>
2026-02-16 08:56:18 +0400

    new post

--- .astro/content-modules.mjs
+++ .astro/content-modules.mjs
@@ -1,11 +1,58 @@
-
 export default new Map([
-["content/posts/2026-02-13-knowledge-is-power-or.mdx", () => import("astro:content-layer-deferred-module?astro%3Acontent-layer-deferred-module=&fileName=content%2Fposts%2F2026-02-13-knowledge-is-power-or.mdx&astroContentModuleFlag=true")],
-["content/posts/2023-01-04-new-year-resolutions.mdx", () => import("astro:content-layer-deferred-module?astro%3Acontent-layer-deferred-module=&fileName=content%2Fposts%2F2023-01-04-new-year-resolutions.mdx&astroContentModuleFlag=true")],
-["content/posts/2023-02-01-frontend-interview.mdx", () => import("astro:content-layer-deferred-module?astro%3Acontent-layer-deferred-module=&fileName=content%2Fposts%2F2023-02-01-frontend-interview.mdx&astroContentModuleFlag=true")],
-["content/posts/2023-01-11-first-week-in.mdx", () => import("astro:content-layer-deferred-module?astro%3Acontent-layer-deferred-module=&fileName=content%2Fposts%2F2023-01-11-first-week-in.mdx&astroContentModuleFlag=true")],
-["content/posts/2023-02-09-full-on-business.mdx", () => import("astro:content-layer-deferred-module?astro%3Acontent-layer-deferred-module=&fileName=content%2Fposts%2F2023-02-09-full-on-business.mdx&astroContentModuleFlag=true")],
-["content/posts/2025-10-21-boring-or-fancy.mdx", () => import("astro:content-layer-deferred-module?astro%3Acontent-layer-deferred-module=&fileName=content%2Fposts%2F2025-10-21-boring-or-fancy.mdx&astroContentModuleFlag=true")],
-["content/pages/about.mdx", () => import("astro:content-layer-deferred-module?astro%3Acontent-layer-deferred-module=&fileName=content%2Fpages%2Fabout.mdx&astroContentModuleFlag=true")],
-["content/posts/2026-01-13-interview-project-update.mdx", () => import("astro:content-layer-deferred-module?astro%3Acontent-layer-deferred-module=&fileName=content%2Fposts%2F2026-01-13-interview-project-update.mdx&astroContentModuleFlag=true")]]);
-		
+  [
+    "content/posts/2026-02-13-knowledge-is-power-or.mdx",
+    () =>
+      import(
+        "astro:content-layer-deferred-module?astro%3Acontent-layer-deferred-module=&fileName=content%2Fposts%2F2026-02-13-knowledge-is-power-or.mdx&astroContentModuleFlag=true"
+      ),
+  ],
+  [
+    "content/posts/2023-01-04-new-year-resolutions.mdx",
+    () =>
+      import(
+        "astro:content-layer-deferred-module?astro%3Acontent-layer-deferred-module=&fileName=content%2Fposts%2F2023-01-04-new-year-resolutions.mdx&astroContentModuleFlag=true"
+      ),
+  ],
+  [
+    "content/posts/2023-02-01-frontend-interview.mdx",
+    () =>
+      import(
+        "astro:content-layer-deferred-module?astro%3Acontent-layer-deferred-module=&fileName=content%2Fposts%2F2023-02-01-frontend-interview.mdx&astroContentModuleFlag=true"
+      ),
+  ],
+  [
+    "content/posts/2023-01-11-first-week-in.mdx",
+    () =>
+      import(
+        "astro:content-layer-deferred-module?astro%3Acontent-layer-deferred-module=&fileName=content%2Fposts%2F2023-01-11-first-week-in.mdx&astroContentModuleFlag=true"
+      ),
+  ],
+  [
+    "content/posts/2023-02-09-full-on-business.mdx",
+    () =>
+      import(
+        "astro:content-layer-deferred-module?astro%3Acontent-layer-deferred-module=&fileName=content%2Fposts%2F2023-02-09-full-on-business.mdx&astroContentModuleFlag=true"
+      ),
+  ],
+  [
+    "content/posts/2025-10-21-boring-or-fancy.mdx",
+    () =>
+      import(
+        "astro:content-layer-deferred-module?astro%3Acontent-layer-deferred-module=&fileName=content%2Fposts%2F2025-10-21-boring-or-fancy.mdx&astroContentModuleFlag=true"
+      ),
+  ],
+  [
+    "content/pages/about.mdx",
+    () =>
+      import(
+        "astro:content-layer-deferred-module?astro%3Acontent-layer-deferred-module=&fileName=content%2Fpages%2Fabout.mdx&astroContentModuleFlag=true"
+      ),
+  ],
+  [
+    "content/posts/2026-01-13-interview-project-update.mdx",
+    () =>
+      import(
+        "astro:content-layer-deferred-module?astro%3Acontent-layer-deferred-module=&fileName=content%2Fposts%2F2026-01-13-interview-project-update.mdx&astroContentModuleFlag=true"
+      ),
+  ],
+]);
